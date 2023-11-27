35cb5209c60a168d3d3ed59d974def8bd6674b43
Olena Sheremet <lenka.sheremet1978@gmail.com>
2023-11-27 11:38:12 +0200

    Delete Lab10/ArtilleryFactory.java

--- Lab10/ArtilleryFactory.java
+++
@@ -1,6 +0,0 @@
-public class ArtilleryFactory extends MilitaryUnitFactory { // Фабрика для створення артилерії
-    @Override
-    MilitaryUnit createMilitaryUnit() {
-        return new Artillery();
-    }
-}
