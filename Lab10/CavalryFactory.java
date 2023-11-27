c688c1765e33479574f5f90c8924b76871e87195
Olena Sheremet <lenka.sheremet1978@gmail.com>
2023-11-27 11:38:33 +0200

    Delete Lab10/CavalryFactory.java

--- Lab10/CavalryFactory.java
+++
@@ -1,6 +0,0 @@
-public class CavalryFactory extends MilitaryUnitFactory {// Фабрика для створення кінноти
-    @Override
-    MilitaryUnit createMilitaryUnit() {
-        return new Cavalry();
-    }
-}
